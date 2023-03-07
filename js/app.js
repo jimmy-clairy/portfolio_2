cc736523fcb95ff6e7585655ccc6a38ca61403a5
Jimmy Clairy <jimcl@hotmail.fr>
2023-03-07 17:20:22 +0100

    end first section start second

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Effect slide
+// Effect nav-slider
 const btnToggle = document.querySelector(".btn-toggle")
 const navSlider = document.querySelector(".nav-slider")
 const navSliderLink = document.querySelectorAll(".nav-slider__link")
@@ -13,9 +13,28 @@ function toggleNav() {
 
 // Effect multiText
 const typingEffect = new Typed(".intro__multiText", {
-    strings: ["Coder", "Jimmy Clairy"],
+    strings: ["Developer", "Jimmy Clairy"],
     loop: true,
     typeSpeed: 100,
     backSpeed: 80,
     backDelay: 1500
+})
+
+
+// Effect slide
+const slideProjets = document.querySelector('.projets');
+
+window.addEventListener('scroll', () => {
+
+    const { scrollTop, clientHeight } = document.documentElement;
+
+    // console.log(scrollTop, clientHeight);
+
+    const topElementToTopViewport = slideProjets.getBoundingClientRect().top;
+
+    // console.log(topElementToTopViewport);
+
+    if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.9) {
+        slideProjets.classList.add('active')
+    }
 })
