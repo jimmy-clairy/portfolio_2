2b6835cd895159aeea85ec61963197fb69a3dc66
Jimmy Clairy <jimcl@hotmail.fr>
2023-06-05 18:58:02 +0200

    Add jsDoc

--- js/fetch/fetchData.js
+++ js/fetch/fetchData.js
@@ -1,7 +1,19 @@
+/**
+ * Récupère des données à partir d'une URL en utilisant la méthode Fetch.
+ * @param {string} url - L'URL à partir de laquelle récupérer les données.
+ * @returns {Promise} - Une promesse contenant les données au format JSON.
+ * @throws {Error} - Une erreur est lancée si le serveur rencontre un problème.
+ */
 export async function fetchData(url) {
-    const res = await fetch(url)
+    /**
+     * Effectue une requête Fetch pour récupérer les données.
+     * @type {Response}
+     */
+    const res = await fetch(url);
+
     if (res.ok) {
-        return res.json()
+        return res.json();
     }
-    throw new Error("Probleme serveur")
+
+    throw new Error("Problème serveur");
 }
--- js/functions/functions.js
+++ js/functions/functions.js
@@ -1,65 +1,116 @@
-// Effect nav-slider ---------------------------------------------------------------------------------------------
+/**
+ * Active le slider de navigation.
+ */
 export function sliderNav() {
-    const btnToggle = document.querySelector(".btn-toggle")
-    const navSlider = document.querySelector(".nav-slider")
-    const navSliderLink = document.querySelectorAll(".nav-slider__link")
+    /**
+     * Le bouton de bascule du slider.
+     * @type {HTMLElement}
+     */
+    const btnToggle = document.querySelector(".btn-toggle");
 
-    btnToggle.addEventListener("click", toggleNav)
-    navSliderLink.forEach(a => a.addEventListener("click", () => toggleNav()))
+    /**
+     * Le slider de navigation.
+     * @type {HTMLElement}
+     */
+    const navSlider = document.querySelector(".nav-slider");
 
+    /**
+     * Les liens du slider de navigation.
+     * @type {NodeList}
+     */
+    const navSliderLink = document.querySelectorAll(".nav-slider__link");
+
+    btnToggle.addEventListener("click", toggleNav);
+    navSliderLink.forEach(a => a.addEventListener("click", () => toggleNav()));
+
+    /**
+     * Bascule l'état du slider de navigation.
+     */
     function toggleNav() {
-        btnToggle.classList.toggle("active")
-        navSlider.classList.toggle("active")
+        btnToggle.classList.toggle("active");
+        navSlider.classList.toggle("active");
     }
 }
 
-// ThemeMode -------------------------------------------------------------------------------------------------------
+/**
+ * Active le mode sombre/lumineux.
+ */
 export function themeMode() {
-    const body = document.body
-    const sun = document.querySelector('.sun')
-    const moon = document.querySelector('.moon')
+    /**
+     * Le corps du document.
+     * @type {HTMLElement}
+     */
+    const body = document.body;
 
+    /**
+     * L'icône représentant le soleil.
+     * @type {HTMLElement}
+     */
+    const sun = document.querySelector('.sun');
+
+    /**
+     * L'icône représentant la lune.
+     * @type {HTMLElement}
+     */
+    const moon = document.querySelector('.moon');
+
+    /**
+     * Active le mode sombre.
+     */
     function darkMode() {
-        localStorage.setItem('darkMode', 'dark')
-        moon.style.display = 'none'
-        sun.style.display = 'block'
-        body.classList.add('dark')
+        localStorage.setItem('darkMode', 'dark');
+        moon.style.display = 'none';
+        sun.style.display = 'block';
+        body.classList.add('dark');
     }
 
+    /**
+     * Active le mode lumineux.
+     */
     function lightMode() {
-        localStorage.setItem('darkMode', 'light')
-        sun.style.display = 'none'
-        moon.style.display = 'block'
-        body.classList.remove('dark')
+        localStorage.setItem('darkMode', 'light');
+        sun.style.display = 'none';
+        moon.style.display = 'block';
+        body.classList.remove('dark');
     }
 
-    const themeStorage = localStorage.getItem('darkMode')
+    const themeStorage = localStorage.getItem('darkMode');
+
     if (themeStorage === 'dark') {
-        darkMode()
+        darkMode();
     } else {
-        lightMode()
+        lightMode();
     }
 
-    const theme = document.querySelector('.theme')
+    /**
+     * Écouteur d'événement pour basculer entre les modes sombre et lumineux.
+     */
+    const theme = document.querySelector('.theme');
     theme.addEventListener('click', () => {
-        const themeStorage = localStorage.getItem('darkMode')
+        const themeStorage = localStorage.getItem('darkMode');
         if (themeStorage === 'dark') {
-            lightMode()
+            lightMode();
         } else {
-            darkMode()
+            darkMode();
         }
-    })
+    });
 }
 
-// Effect header ---------------------------------------------------------------------------------------------
+/**
+ * Ajoute l'effet à l'en-tête.
+ */
 export function headerEffect() {
+    /**
+     * L'en-tête du document.
+     * @type {HTMLElement}
+     */
     const header = document.querySelector('.header');
+
     window.addEventListener('scroll', () => {
-        // console.log(window.scrollY);
         if (window.scrollY > 20) {
-            header.classList.add("active")
+            header.classList.add("active");
         } else {
-            header.classList.remove("active")
+            header.classList.remove("active");
         }
-    })
+    });
 }
